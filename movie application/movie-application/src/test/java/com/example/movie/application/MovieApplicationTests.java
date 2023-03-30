43bce5b28a7c90755cda803f7969d1a26006c3e1
Naveenchelluri <92864484+Naveenchelluri@users.noreply.github.com>
2023-03-30 17:02:28 +0530

    Update MovieApplicationTests.java

--- movie application/movie-application/src/test/java/com/example/movie/application/MovieApplicationTests.java
+++ movie application/movie-application/src/test/java/com/example/movie/application/MovieApplicationTests.java
@@ -9,5 +9,19 @@ class MovieApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	
+	 @Test
+	    public void findallTest() {
+		   
+		   Movie m1 = new Movie(1,"kushi","pawan kalyan","sj");
+		   Movie m2 = new Movie(2,"kushi2","pawan kalyann","sjj");
+		   
+		   List<Movie> list1 = new ArrayList<>();
+		   list1.add(m1);
+		   list1.add(m2);
+		   
+		   when(movieRepository.findAll()).thenReturn(list1);
+		   assertEquals(movieService.findAll().size(),2);
 
 }
